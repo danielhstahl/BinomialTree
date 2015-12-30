961f501e56f26606aa9f2c12023ffba73c7d3b55
phillyfan1138 <danstahl1138@gmail.com>
2015-12-30 11:46:47 -0500

    defaults to american

--- Tree.h
+++ Tree.h
@@ -4,65 +4,28 @@
 #include <cmath>
 #include <unordered_map>
 #include <vector>
-class Tree{ //simulates SDE of type dX=alpha(t, X)dt+sigma(t, X)dW...I think I need to figure out dividends!
-  private:
-    int m;//number of steps
-    double maturity;//maturity in years
-    double dt;
-    double x0;
-    double finalValue;
-    int numberOfPChanged=0;
-    std::unordered_map<int, bool> exerciseNodes;
-    std::vector<double> optionPrice;
-  public:
-    Tree(int, double, double);
-    Tree(int, double, double, std::unordered_map<int, bool>&); //include exercise nodes...use this in conjuctions with a date module!
-    Tree(int, double, double, bool);//american or not
-    template< typename ALPHA, typename SIGMA, typename FINV, typename PAYOFF, typename DISCOUNT>
-    void execute(ALPHA&& alpha,  SIGMA&& sigma,  FINV&& fInv, PAYOFF&& payoff, DISCOUNT&& discount){ //alpha=alpha/sigma,sigma=sigma'(x), finv=the inverse of the function g=\int 1/sig(x) dx
-      double p=0;
-      double t=(m-1)*dt;
-      double alph;
-      double sig;
-      double underlyingX=0;
-      double intrinsic=0;
-      int mm=m;
-      double sqrtDt=sqrt(dt);
-      for(int i=0; i<mm; i++){
-        underlyingX=fInv(x0+sqrtDt*(mm-1-2*i));
-        optionPrice[i]=payoff(underlyingX);
-      }
-      mm--;
-      for(int j=(m-2); j>=0; j--){
-        t=j*dt;
-        for(int i=0; i<mm; i++){
-          underlyingX=fInv(x0+sqrtDt*(mm-1-2*i));
-          alph=alpha(t, underlyingX);
-          sig=sigma(t, underlyingX);
-          p=(alph-sig*.5)*.5*sqrtDt+.5;
-          if(p>1){
-            p=1;
-            numberOfPChanged++;
-          }
-          else if(p<0){
-            p=0;
-            numberOfPChanged++;
-          }
-          optionPrice[i]=(p*optionPrice[i]+(1-p)*optionPrice[i+1])*discount(t, underlyingX, dt);
-          if(exerciseNodes.find(i)!=exerciseNodes.end()){ //if the key exists
-            if(exerciseNodes.at(i)){ //if the option can be exercised
-              intrinsic=payoff(underlyingX);
-              if(optionPrice[i]<intrinsic){
-                optionPrice[i]=intrinsic;
-              }
-            }
-          }
-        }
-        optionPrice.pop_back();
-        mm--;
-      }
-      finalValue=optionPrice[0];
-    }
-    double getPrice();
-};
+template<typename Number>
+auto execute(
+    auto&,  //function(t, x) for the drift ->alpha=alpha/sigma
+    auto&,  //function(t, x) for the volatility->sigma=sigma'(x)
+    auto&, //inverse of the function g=\int 1/sig(x) dx
+    auto&, //payoff function(x)
+    auto&, //discount function(t, x, dt)
+    int, //number of nodes
+    double, //time
+    Number&, //current value
+    std::unordered_map<int, bool>& //nodes to exercise at
+); 
+template<typename Number>
+auto execute(
+    auto&,  //function(t, x) for the drift ->alpha=alpha/sigma
+    auto&,  //function(t, x) for the volatility->sigma=sigma'(x)
+    auto&, //inverse of the function g=\int 1/sig(x) dx
+    auto&, //payoff function(x)
+    auto&, //discount function(t, x, dt)
+    int, //number of nodes
+    double, //time
+    Number& //current value
+); 
+#include "Tree.hpp"
 #endif
---
+++ Tree.hpp
@@ -0,0 +1,82 @@
+//template< typename ALPHA, typename SIGMA, typename FINV, typename PAYOFF, typename DISCOUNT>
+template<typename Number>
+auto execute(auto& alpha,  auto& sigma,  auto& fInv, auto& payoff, auto& discount, int m, double t, const Number& x0, std::unordered_map<int, bool>& exerciseNodes){ //alpha=alpha/sigma,sigma=sigma'(x), finv=the inverse of the function g=\int 1/sig(x) dx
+    //double t=(m-1)*dt;
+    double dt=t/(m-1);
+    std::vector<Number> optionPrice(m);
+    int mm=m;
+    double sqrtDt=sqrt(dt);
+    for(int i=0; i<mm; i++){
+        auto underlyingX=fInv(t, x0+sqrtDt*(mm-1-2*i), dt, mm);
+        optionPrice[i]=payoff(t, underlyingX, dt, mm);
+    }
+    mm--;
+    for(int j=(m-2); j>=0; j--){
+        t=j*dt;
+        for(int i=0; i<mm; i++){
+            auto underlyingX=fInv(t, x0+sqrtDt*(mm-1-2*i), dt, mm);
+            auto alph=alpha(t, underlyingX, dt, mm);
+            auto sig=sigma(t, underlyingX, dt, mm);
+            auto p=(alph-sig*.5)*.5*sqrtDt+.5;
+            if(p>1){
+                p=1;
+                //numberOfPChanged++;
+            }
+            else if(p<0){
+                p=0;
+                //numberOfPChanged++;
+            }
+            optionPrice[i]=(p*optionPrice[i]+(1-p)*optionPrice[i+1])*discount(t, underlyingX, dt, mm);
+            if(exerciseNodes.find(i)!=exerciseNodes.end()){ //if the key exists
+                if(exerciseNodes[i]){ //if the option can be exercised
+                    auto intrinsic=payoff(t, underlyingX, dt, mm);
+                    if(optionPrice[i]<intrinsic){
+                        optionPrice[i]=intrinsic;
+                    }
+                }
+            }
+        }
+        optionPrice.pop_back();
+        mm--;
+    }
+    return optionPrice[0];
+}
+
+template<typename Number> //american option
+auto execute(auto& alpha,  auto& sigma,  auto& fInv, auto& payoff, auto& discount, int m, double t, const Number& x0){ //alpha=alpha/sigma,sigma=sigma'(x), finv=the inverse of the function g=\int 1/sig(x) dx
+    double dt=t/(m-1);
+    std::vector<Number> optionPrice(m);
+    int mm=m;
+   // int numberOfPChanged=0;
+    double sqrtDt=sqrt(dt);
+    for(int i=0; i<mm; i++){
+        auto underlyingX=fInv(t, x0+sqrtDt*(mm-1-2*i), dt, mm);
+        optionPrice[i]=payoff(t, underlyingX, dt, mm);
+    }
+    mm--;
+    for(int j=(m-2); j>=0; j--){
+        t=j*dt;
+        for(int i=0; i<mm; i++){
+            auto underlyingX=fInv(t, x0+sqrtDt*(mm-1-2*i), dt, mm);
+            auto alph=alpha(t, underlyingX, dt, mm);
+            auto sig=sigma(t, underlyingX, dt, mm);
+            auto p=(alph-sig*.5)*.5*sqrtDt+.5;
+            if(p>1){
+                p=1;
+                //numberOfPChanged++;
+            }
+            else if(p<0){
+                p=0;
+                //numberOfPChanged++;
+            }
+            optionPrice[i]=(p*optionPrice[i]+(1-p)*optionPrice[i+1])*discount(t, underlyingX, dt, mm);
+            auto intrinsic=payoff(t, underlyingX, dt, mm);
+            if(optionPrice[i]<intrinsic){
+                optionPrice[i]=intrinsic;
+            }
+        }
+        optionPrice.pop_back();
+        mm--;
+    }
+    return optionPrice[0];
+}
